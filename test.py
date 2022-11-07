f579d5907dbcda0c17d5063154c934b829bb2d86
Francisco-1453 <you@example.com>
2022-11-07 17:18:49 -0300

    tiempo

--- test.py
+++ test.py
@@ -2,3 +2,11 @@ import sched
 import time
 
 s = sched.scheduler(time.time, time.sleep)
+def print_time():
+    print(time.time())
+
+def print_some_time():
+    s.enter(5,1,print_time)
+    s.run()
+
+print_some_time()
