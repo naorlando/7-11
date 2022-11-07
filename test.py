5e23640fb956faa136cb02e227c7162a62a5bacb
Nahuel <naorlando@frba.utn.edu.ar>
2022-11-07 17:10:39 -0300

    suma

--- test.py
+++ test.py
@@ -2,3 +2,14 @@ import sched
 import time
 
 s = sched.scheduler(time.time, time.sleep)
+
+
+def sum(a,b):
+    print (a+b)
+
+def print_in_time():
+    s.enter(3,1,sum,argument=(7,2))
+    s.run()
+
+
+print_in_time()
